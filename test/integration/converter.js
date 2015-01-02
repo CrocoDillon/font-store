cb1bc1e23f8fb7e9ea332c10bbfe94256637d63b
Christian Fei <crifei93@gmail.com>
2015-01-02 19:32:41 +0100

    refactored code to separate components converter, added more tests

--- test/integration/converter.js
+++ test/integration/converter.js
@@ -1,14 +1,14 @@
 var expect = require('chai').expect
 var fs = require('fs');
 
-var fontStore = require('../../lib/');
+var converter = require('../../lib/converter');
 var dosisJson = require('../fonts-fixtures/Dosis.json');
 
 var createdFile;
 
-describe('font-store', function () {
+describe('converter', function () {
   it('should convert the font Dosis to json', function (done) {
-    fontStore('http://fonts.googleapis.com/css?family=Dosis', function(fontJson){
+    converter.convert('http://fonts.googleapis.com/css?family=Dosis', function(fontJson){
       createdFile=fontJson.md5;
       expect(fontJson.md5).to.equal(dosisJson.md5);
       done();
@@ -18,7 +18,7 @@ describe('font-store', function () {
   afterEach(function() {
     if( createdFile ){
       fs.unlink('fonts.'+createdFile+'.woff.json', function (err) {
-        if( err ) console.log( err );
+        if( err && err.code !== 'ENOENT' ) console.log( err );
       });
     }
     createdFile=null;
---
+++ test/integration/fontStore.js
@@ -0,0 +1,24 @@
+var fontStore = require('../../lib/fontStore');
+var expect = require('chai').expect
+
+var fs = require('fs');
+
+var createdFile;
+
+describe('fontStore', function () {
+  it('should generate a file called ... for the font Dosis', function (done) {
+    fontStore('http://fonts.googleapis.com/css?family=Dosis', function(err,fileName,json){
+      var fileExists = fs.existsSync(fileName)
+      expect(fileExists).to.be.true;
+      createdFile=fileName;
+      done();
+    });
+  });
+
+  afterEach(function() {
+    if( createdFile )
+      fs.unlink(createdFile)
+    createdFile=null;
+  });
+
+});
