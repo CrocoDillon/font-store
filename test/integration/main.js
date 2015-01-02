17740b90fff06eff0db0e9ec6251b1e652a487ac
Christian Fei <crifei93@gmail.com>
2015-01-02 19:05:30 +0100

    clean up file after integration test

--- test/integration/main.js
+++ test/integration/main.js
@@ -1,12 +1,26 @@
 var expect = require('chai').expect
+var fs = require('fs');
+
 var fontStore = require('../../lib/');
 var dosisJson = require('../fonts-fixtures/Dosis.json');
+
+var createdFile;
 
 describe('font-store', function () {
   it('should convert the font Dosis to json', function (done) {
     fontStore('http://fonts.googleapis.com/css?family=Dosis', function(err,fontJson){
+      createdFile=fontJson.md5;
       expect(fontJson.md5).to.equal(dosisJson.md5);
       done();
     });
+  });
+
+  afterEach(function() {
+    if( createdFile ){
+      fs.unlink('fonts.'+createdFile+'.woff.json', function (err) {
+        if( err ) console.log( err );
+      });
+    }
+    createdFile=null;
   });
 });
