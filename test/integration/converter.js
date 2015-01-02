98f470b7026d448826e4c318c5c1b20b2aa0b3ba
Christian Fei <crifei93@gmail.com>
2015-01-02 19:40:03 +0100

    more granular tests for converter

--- test/integration/converter.js
+++ test/integration/converter.js
@@ -7,11 +7,20 @@ var dosisJson = require('../fonts-fixtures/Dosis.json');
 var createdFile;
 
 describe('converter', function () {
-  it('should convert the font Dosis to json', function (done) {
-    converter.convert('http://fonts.googleapis.com/css?family=Dosis', function(fontJson){
-      createdFile=fontJson.md5;
-      expect(fontJson.md5).to.equal(dosisJson.md5);
-      done();
+  describe('conversion of font Dosis', function () {
+    it('should match the md5 sum', function (done) {
+      converter.convert('http://fonts.googleapis.com/css?family=Dosis', function(fontJson){
+        createdFile=fontJson.md5;
+        expect(fontJson.md5).to.equal(dosisJson.md5);
+        done();
+      });
+    });
+    it('should match the converted font', function (done) {
+      converter.convert('http://fonts.googleapis.com/css?family=Dosis', function(fontJson){
+        createdFile=fontJson.md5;
+        expect(fontJson.value).to.equal(dosisJson.value);
+        done();
+      });
     });
   });
 
