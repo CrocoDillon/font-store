0246fa6e5bcf69b3eaff8cb52329754c69a05e32
Dillon de Voor <dillon@crocodillon.com>
2015-01-07 19:40:50 +0100

    Move replaceWithUrlEncodedContent function to utils
    Also add support for quotes URLs and update tests.

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -1,11 +1,35 @@
-var expect = require('chai').expect;
-var utils = require('../../lib/modules/utils');
+var expect    = require('chai').expect,
+    utils     = require('../../lib/modules/utils'),
+    constants = require('../../lib/constants');
 
-var url = 'http://google.com';
-var cssString = "@font-face {font-family: 'Dosis';src: local('Dosis Regular'), local('Dosis-Regular'), url("+url+")";
-describe('utils', function () {
-  it('should extract urls found in a string', function () {
-    var extractedUrls = utils.extractRegExp(cssString, /url\((.*?)\)/g);
-    expect(extractedUrls).to.deep.equal([url]);
+var url = 'http://google.com',
+    cssStringNoQuotes     = '@font-face {font-family: \'Dosis\';src: local(\'Dosis Regular\'), local(\'Dosis-Regular\'), url('   + url +   ')',
+    cssStringSingleQuotes = '@font-face {font-family: \'Dosis\';src: local(\'Dosis Regular\'), local(\'Dosis-Regular\'), url(\'' + url + '\')',
+    cssStringDoubleQuotes = '@font-face {font-family: \'Dosis\';src: local(\'Dosis Regular\'), local(\'Dosis-Regular\'), url("'  + url +  '")';
+
+describe('utils', function() {
+  describe('extractRegExp', function() {
+    it('should extract urls found in a css string', function () {
+      var extractedUrls = utils.extractRegExp(cssStringNoQuotes, constants.urlFormat);
+      expect(extractedUrls).to.deep.equal([url]);
+    });
+    it('should also work with single quotes', function () {
+      var extractedUrls = utils.extractRegExp(cssStringSingleQuotes, constants.urlFormat);
+      expect(extractedUrls).to.deep.equal([url]);
+    });
+    it('should also work with double quotes', function () {
+      var extractedUrls = utils.extractRegExp(cssStringDoubleQuotes, constants.urlFormat);
+      expect(extractedUrls).to.deep.equal([url]);
+    });
+  });
+
+  describe('replaceWithUrlEncodedContent', function() {
+    it('should replace urls with base64 encoded content', function () {
+      var result = utils.replaceWithUrlEncodedContent(cssStringNoQuotes, url, 'BASE64DATA', 'CONTENTTYPE'),
+          indexOfUrl  = result.indexOf(url),
+          indexOfData = result.indexOf('BASE64DATA');
+      expect(indexOfUrl).to.equal(-1);
+      expect(indexOfData).to.not.equal(-1);
+    });
   });
 });
